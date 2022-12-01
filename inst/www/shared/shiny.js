96f98911d927b6af6ddcce3ec7a83dd63643d541
dvg-p4 <dgealow@paradigm4.com>
2022-12-01 17:02:39 -0500

    actually compile

--- inst/www/shared/shiny.js
+++ inst/www/shared/shiny.js
@@ -1,4 +1,4 @@
-/*! shiny 1.7.2.3999 | (c) 2012-2022 RStudio, PBC. | License: GPL-3 | file LICENSE */
+/*! shiny 1.7.2.9002 | (c) 2012-2022 RStudio, PBC. | License: GPL-3 | file LICENSE */
 (function() {
   var __create = Object.create;
   var __defProp = Object.defineProperty;
@@ -10508,6 +10508,7 @@
       coords.direction = opts.brushDirection;
       coords.brushId = inputId;
       coords.outputId = outputId;
+      console.log("Sending brush input data to server, somehow");
       shinySetInputValue(inputId, coords);
       $el.data("mostRecentBrush", true);
       imageOutputBinding.find(document.documentElement).trigger("shiny-internal:brushed", coords);
@@ -13224,7 +13225,7 @@
   var windowShiny2;
   function setShiny(windowShiny_) {
     windowShiny2 = windowShiny_;
-    windowShiny2.version = "1.7.2.3999";
+    windowShiny2.version = "1.7.2.9002";
     var _initInputBindings = initInputBindings(), inputBindings = _initInputBindings.inputBindings, fileInputBinding2 = _initInputBindings.fileInputBinding;
     var _initOutputBindings = initOutputBindings(), outputBindings = _initOutputBindings.outputBindings;
     setFileInputBinding(fileInputBinding2);
